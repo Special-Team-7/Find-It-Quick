3274a55392b2a4e3dc9d2c0acd88c09becac7441
ChibiKev <k3v.ch3n@gmail.com>
2019-10-25 13:30:07 -0400

    Added Bootstrap CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import { 
   BrowserRouter as Router, 
   Switch, 
-  Route, 
+  Route,
   Link,
-  NavLink
+  NavLink,
+  NavbarBrand
 } from 'react-router-dom';
 
 import PostsListPage from './pages/PostsListPage';
@@ -17,11 +19,35 @@ import './App.css';
 
 function Navigation(props) {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+
+
+//   <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+//   <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+//   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//   <Navbar.Collapse id="responsive-navbar-nav">
+//     <Nav className="mr-auto">
+//       <Nav.Link href="#features">Features</Nav.Link>
+//       <Nav.Link href="#pricing">Pricing</Nav.Link>
+//       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+//         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+//         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//         <NavDropdown.Divider />
+//         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+//       </NavDropdown>
+//     </Nav>
+//     <Nav>
+//       <Nav.Link href="#deets">More deets</Nav.Link>
+//       <Nav.Link eventKey={2} href="#memes">
+//         Dank memes
+//       </Nav.Link>
+//     </Nav>
+//   </Navbar.Collapse>
+// </Navbar>
+    <nav collapseOnSelect className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
       <Link className="navbar-brand" to="/">Micro Blog</Link>
+      {/* <NavbarToggle aria-controls="responsive-navbar-nav" />
+      <NavbarCollapse id="responsive-navbar-nav"></NavbarCollapse> */}
       <ul className="navbar-nav mr-auto">
         <li className="nav-item">
           <NavLink className="nav-link" exact to="/posts/new">
