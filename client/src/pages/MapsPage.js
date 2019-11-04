1ca763e6909cd53a31feb480e86ab3ba1e29d9ad
Anthony Bravo <abravo3641@gmail.com>
2019-11-04 18:17:28 -0500

    Working map and fake bathrooms routed completed

--- client/src/pages/MapsPage.js
+++ client/src/pages/MapsPage.js
@@ -2,22 +2,26 @@ import React from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 const mapStyles = {
-  width: '50%',
-  height: '50%'
+  width: '100%',
+  height: '100%'
 };
 
 export class MapsPage extends React.Component {
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: 40.7831,
-         lng: -73.9712
-        }}
-      />
+      <div className="container-fluid text-center">
+        <div className="row justify-content-center">
+          <Map
+            google={this.props.google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={{
+            lat: 40.7831,
+            lng: -73.9712
+            }}
+          />
+        </div>
+      </div>
     );
   }
 }
