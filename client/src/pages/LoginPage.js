ea270fef1b72003b631e74c577aa7772d8c90cfa
Anthony Bravo <abravo3641@gmail.com>
2019-11-06 17:06:18 -0500

    broken

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
 
 function LoginPage(props) {
   return (
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -3,18 +3,32 @@ import * as firebase from 'firebase/app';
 
 
 export default class RegisterPage extends React.Component {
-    state = {
+      state = {
         name: "",
         email: "",
         password:""
       };
-    
+      
+      //Create user in Firebase
       createUser = (e) => {
+        console.log(`Email: ${this.state.email} Pass: ${this.state.password}`);
         firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
           var errorCode = error.code;
           var errorMessage = error.message;
           console.log(errorMessage)
         });
+      }
+
+      emailChanged = (e) => {
+        this.setState({
+          email: e.target.value
+        })
+      }
+
+      passChanged = (e) => {
+        this.setState({
+          password: e.target.value
+        })
       }
     
     render() {
@@ -35,11 +49,11 @@ export default class RegisterPage extends React.Component {
             {
                 user
                 ? <button onClick={signOut}>Sign out</button>
-                : <button onClick={this.createUser}>Sign in with Google</button>
+                : <button onClick={this.createUser}>Sign Up</button>
             }
             <form>
-              {"Email:"}<input type="text" placeholder="no-reply@finditquick.com" ></input>
-              {"Password"}<input type="password" placeholder="*********"></input>
+              {"Email:"}<input type="text" placeholder="no-reply@finditquick.com" onChange={this.emailChanged}></input>
+              {"Password"}<input type="password" placeholder="*********" onChange={this.passChanged}></input>
             </form>
 
             </header>
