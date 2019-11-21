1118fd6be7336125c3d694c41b86e692bf766e19
josue <josueh6758@gmail.com>
2019-11-20 20:51:39 -0500

    Added functional api routes for bathroom

--- api/controllers/bathroom.js
+++ api/controllers/bathroom.js
@@ -1,33 +1,41 @@
 const express = require('express');
 var fakeBathrooms = require('../fakeBathrooms');
 const router = express.Router();
+const {Bathroom,Post,User} = require('../models');
 //const { Post } = db;
 
 
 
 //get all bathrooms
 router.get('/', (req,res) => {
-    
-
-
-
-    res.json(fakeBathrooms);
+    Bathroom.findAll({
+    }).then(bathroom => {
+        if(!bathroom){
+            res.send("No Bathrooms Found!");
+        }
+        console.log(bathroom);
+        res.json(bathroom);
+    })
 })
 
 //get reviews for a bathroom
 router.get('/:id', (req,res) => {
-    res.json("workin");
+    console.log(`calling specific id! ${req.params.id}`);
+    bid=req.params.id;
+    Bathroom.findOne({
+        where: {
+            id: bid
+        }
+    }).then(bathroom => {
+        if(!bathroom){
+            res.sendStatus(404);
+        }
+        console.log(bathroom);
+        res.json(bathroom);
+    })
 });
 
-//get user profile
-router.get('/:userid', (req,res) => {
 
-});
-
-//post a review for a bathroom 
-router.post('/', (req,res) => {
-    
-});
 
 //post a new bathroom location
 router.post('/', (req,res) => {
