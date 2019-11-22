400af798ad3983537f24ba67a84d4b84e68fa060
josue <josueh6758@gmail.com>
2019-11-22 16:05:21 -0500

    Changed bathroom shema fixing controllers

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const {Bathroom,Post,User} = require('../models');
 
 
 //get all users
@@ -7,18 +8,35 @@ router.get('/', (req,res) => {
     res.send('working');
 });
 
+// router.post('/register', (req,res) => {
+//     let user = {
+//         id: req.body.id,
+//         name: req.body.name,
+//         email: req.body.email
+//     };
+//     //To do: Insert this user object into postgress using sequealize
+//     User.create({id:id, name:name, email:email})
+//     .then(task => {
+//         res.sendStatus(200);
+//         console.log(`Looks good?: ${task}`);
+//     }).catch(err=>{
+//         console.log("something went wrong!");
+//         res.sendStatus(400);
+//     })
+//     //To do: Send a response to front end to let it know if creation of user
+//     //was a success or not
+// });
+
 router.post('/register', (req,res) => {
     let user = {
         id: req.body.id,
         name: req.body.name,
         email: req.body.email
-    };
-    //To do: Insert this user object into postgress using sequealize
-    console.log(user);
+     };
 
-    //To do: Send a response to front end to let it know if creation of user
-    //was a success or not
-    res.send(user);
+     res.send(req.body);
+     console.log(req.body);
+
 });
 
 module.exports = router; 
