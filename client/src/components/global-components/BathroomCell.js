a6024fffb580655c05f88e2f21355d066aa31c16
Anthony Bravo <abravo3641@gmail.com>
2019-12-06 16:44:27 -0500

    Fixed add bathroom and cells

--- client/src/components/global-components/BathroomCell.js
+++ client/src/components/global-components/BathroomCell.js
@@ -7,6 +7,7 @@ import {Link} from 'react-router-dom';
 function BathroomCell(bathroom) {
     {bathroom = bathroom.bathroom};
     //Construct the bathroom URL for street view
+    console.log(bathroom)
     bathroom.url = `https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${bathroom.latitude},${bathroom.longitude}&fov=80&heading=180&pitch=0&key=`+process.env.REACT_APP_GOOGLE_MAPS_KEY;
     return(
         <div className="container">
@@ -16,13 +17,15 @@ function BathroomCell(bathroom) {
             </div>
             <div className="col-9">
               <div className='col'>
+                <p>
                 <Link to={{
                   pathname:  `/bathroom/${bathroom.id}`,
                   state: {id: bathroom.id}
                 }}> Name: {bathroom.name} </Link>
+                </p>
               </div>
               <div className='col'>
-                <p>Rating: {bathroom.average_rating} </p>
+                <p>Rating: {bathroom.rating} </p>
               </div>
               <div className='col'>
                 <p>Category: {bathroom.category} </p>
@@ -32,6 +35,8 @@ function BathroomCell(bathroom) {
               </div>
            </div>
           </div>
+          <br/>
+          <hr></hr>
         </div>
 
 
