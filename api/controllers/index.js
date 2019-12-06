93405544abb0facb9e153963957461dc7745cb7f
yukkeechang <yukkeechang@gmail.com>
2019-12-06 14:50:55 -0500

    added reviews route, fixed reivews model, edited reviews csv data

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -4,14 +4,17 @@ const router = express.Router();
 
 // Load each controller
 const bathrooms = require('./bathroom');
-const user = require('./user')
+const user = require('./user');
+const review = require('./review');
 const appConfigController = require('./appConfig.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/bathrooms', bathrooms);
 router.use('/user', user);
+router.use('/review', review);
 router.use('/application-configuration', appConfigController);
+
 
 
 module.exports = router;
